6aae4c29f5231d1d0e2c3c1a1f6b4eab445866c7
Elena <kuscha@yandex.ru>
2023-01-29 18:00:27 +0300

    fix

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,23 +1,21 @@
 // Задание 4.1. Вывод карточек товаров
 
 function parseProducts(json) {
-    let data;
-    data  (JSON.parse(json));
-    let products ;
-    products = data.products;
-        return data.products;
-   
-}
-
-function renderProductsCards(json){   
-    clearProducts();
-products  (parseProducts(json));
+    let data = JSON.parse(json);
+    let products = data.products;
+    return products;
+    }
     
-for (let i = 0; i < length; i += 1) {
-length = products.length;
- addProducts (products[i]);  
- }
- } 
+    function renderProductsCards(json) {
+    clearProducts();
+    renderProductsCards (products);
+    products = parseProducts(json);
+    length = products.length;
+    for (let i = 0; i < length; i +=1) {
+    addProduct(products[i]);
+    }
+    }
+    
 
 
  
