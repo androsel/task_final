b722833f9f94c2d1460432f1cd8586ce5f7e5edd
Elena <kuscha@yandex.ru>
2023-01-30 16:51:17 +0300

    fix

--- task_final/solution.js
+++ task_final/solution.js
@@ -44,9 +44,9 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push({title: goods[i].title,count: goods[i].count});
     }
 
-    data.order.address = "ул. " + address.street + ", дом " + address.house + " , " + address.entrance + " подъезд," + address.floor + " этаж, " + "кв. " + address.flat;;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + " , " + address.entrance + " подъезд," + address.floor + " этаж, " + "кв " + address.flat;
     data.order.sum = sum;
-    data.client = sendRequest(name) + " " + sendRequest(phone);
+    data.client = name + " "  + phone;
 
    // data.order.sum = name + client.phone + order.address + goods + order.sum;
 
