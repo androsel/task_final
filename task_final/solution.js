a38b98c7cdbcb3aa70606fd59a4c98aa128c6163
Elena <kuscha@yandex.ru>
2023-01-30 16:18:56 +0300

    fix

--- task_final/solution.js
+++ task_final/solution.js
@@ -47,11 +47,11 @@ function sendRequest(name, phone, address, goods, sum) {
 
     data.order.address =  address;
     data.order.sum = name + phone + address + goods + sum;
-    data.client = name + ' ' + phone;
+   // data.client = name + ' ' + phone;
 
    // data.order.sum = name + client.phone + order.address + goods + order.sum;
 
-  // data.client = 'Иван';
+ data.client = 'Иван';
 
     let jsonData = JSON.stringify({data});
 
