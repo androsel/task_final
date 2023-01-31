496bc841800d60adf729e76b989b63e8d7360cb7
Elena <kuscha@yandex.ru>
2023-01-31 10:47:51 +0300

    fix

--- task_final/solution.js
+++ task_final/solution.js
@@ -35,7 +35,7 @@
 
 function sendRequest(name, phone, address, goods, sum) {
     //let data = {goods: [], order:{}};
-    let data = { client: name + " " + phone, order: { address, sum }, goods: [] };
+    let data = {order: {address, sum}, client: {name, phone}, goods: []}
 
     
 
