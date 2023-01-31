48eb9989c4eed972ce1ab35c1125297179af16b9
Elena <kuscha@yandex.ru>
2023-01-31 10:40:30 +0300

    fix

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,8 +33,9 @@
 //    }
 // }
 
-function sendRequest(name, phone, goods, sum) {
-    let data = {goods: [], order: {}};
+function sendRequest(name, phone, address, goods, sum) {
+    //let data = {goods: [], order:{}};
+    let data = {client: {name, phone},order: {address, sum}, goods: []};
 
     
 
