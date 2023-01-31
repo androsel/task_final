43ad3fbecb4f02dc5f8aea7892d54ef8ecc7d303
Elena <kuscha@yandex.ru>
2023-01-31 10:53:32 +0300

    fix

--- task_final/solution.js
+++ task_final/solution.js
@@ -41,7 +41,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i < countOfGoods; i += 1) {
+    for (let i = 0; i < countOfGoods; i ++) {
         data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
